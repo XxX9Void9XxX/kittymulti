50a5e96d90e1809505d848c1a7204f8942513524
XxX9Void9XxX <brooksm@carbonschools.org>
2025-12-25 16:34:19 -0700

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,12 +1,8 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-function resize() {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-}
-resize();
-addEventListener("resize", resize);
+function resize() { canvas.width = innerWidth; canvas.height = innerHeight; }
+resize(); addEventListener("resize", resize);
 
 const socket = io();
 let state, myId;
@@ -20,12 +16,9 @@ addEventListener("mousemove", e => mouse = e);
 addEventListener("mousedown", () => mouse.down = true);
 addEventListener("mouseup", () => mouse.down = false);
 
-const imgPlayer = new Image();
-imgPlayer.src = "kiitygame.png";
-const imgMouse = new Image();
-imgMouse.src = "mouse.png";
-const imgBird = new Image();
-imgBird.src = "birds.png";
+const imgPlayer = new Image(); imgPlayer.src = "kiitygame.png";
+const imgMouse = new Image(); imgMouse.src = "mouse.png";
+const imgBird = new Image(); imgBird.src = "birds.png";
 
 socket.on("connect", () => myId = socket.id);
 socket.on("state", s => state = s);
@@ -44,21 +37,35 @@ setInterval(() => {
     jump: keys.w || keys[" "] || keys.ArrowUp,
     shoot: mouse.down,
     angle,
-    color: `hsl(${Math.random() * 360},100%,50%)`
+    color: `hsl(${Math.random()*360},100%,50%)`
   });
-}, 1000 / 60);
+}, 1000/60);
 
 socket.on("chat", msg => {
-  const box = document.getElementById("chat");
+  let box = document.getElementById("chat");
+  if (!box) {
+    box = document.createElement("div");
+    box.id = "chat";
+    box.style.position = "absolute";
+    box.style.bottom = "0";
+    box.style.left = "0";
+    box.style.width = "300px";
+    box.style.maxHeight = "200px";
+    box.style.overflowY = "auto";
+    box.style.background = "rgba(0,0,0,0.5)";
+    box.style.color = "white";
+    box.style.fontFamily = "monospace";
+    document.body.appendChild(box);
+  }
   box.innerHTML += msg + "<br>";
   box.scrollTop = box.scrollHeight;
 });
 
 function drawHealth(x, y, w, hp, max) {
   ctx.fillStyle = "red";
-  ctx.fillRect(x, y - 8, w, 5);
+  ctx.fillRect(x, y-8, w, 5);
   ctx.fillStyle = "lime";
-  ctx.fillRect(x, y - 8, w * (hp / max), 5);
+  ctx.fillRect(x, y-8, w*(hp/max), 5);
 }
 
 function loop() {
@@ -69,32 +76,44 @@ function loop() {
   if (!state || !state.players[myId]) return;
 
   const me = state.players[myId];
-  const camX = me.x - canvas.width / 2;
-  const camY = me.y - canvas.height / 2;
+  const camX = me.x - canvas.width/2;
+  const camY = me.y - canvas.height/2;
 
   for (const p of state.platforms) {
     ctx.fillStyle = "#654321";
-    ctx.fillRect(p.x - camX, p.y - camY, p.w, p.h);
+    ctx.fillRect(p.x-camX, p.y-camY, p.w, p.h);
   }
 
   for (const y of state.yarns) {
     ctx.fillStyle = y.color;
     ctx.beginPath();
-    ctx.arc(y.x - camX, y.y - camY, 6, 0, Math.PI * 2);
+    ctx.arc(y.x-camX, y.y-camY, 6, 0, Math.PI*2);
     ctx.fill();
   }
 
   for (const m of state.mice) {
-    ctx.drawImage(imgMouse, m.x - camX, m.y - camY, 40, 40);
-    drawHealth(m.x - camX, m.y - camY, 40, m.hp, m.maxHp);
+    ctx.save();
+    ctx.translate(m.x-camX+20, m.y-camY);
+    ctx.scale(m.vx<0?-1:1,1);
+    ctx.drawImage(imgMouse,-20,0,40,40);
+    ctx.restore();
+    drawHealth(m.x-camX, m.y-camY, 40, m.hp, m.maxHp);
   }
 
   for (const b of state.birds) {
-    ctx.drawImage(imgBird, b.x - camX, b.y - camY, 60, 40);
-    drawHealth(b.x - camX, b.y - camY, 60, b.hp, b.maxHp);
+    ctx.save();
+    ctx.translate(b.x-camX+30, b.y-camY);
+    ctx.scale(b.vx<0?-1:1,1);
+    ctx.drawImage(imgBird,-30,0,60,40);
+    ctx.restore();
+    drawHealth(b.x-camX, b.y-camY, 60, b.hp, b.maxHp);
   }
 
-  ctx.drawImage(imgPlayer, me.x - camX, me.y - camY, 48, 48);
-  drawHealth(me.x - camX, me.y - camY, 48, me.hp, me.maxHp);
+  ctx.save();
+  ctx.translate(me.x-camX+24, me.y-camY+24);
+  ctx.scale(mouse.x < canvas.width/2?-1:1,1);
+  ctx.drawImage(imgPlayer,-24,-24,48,48);
+  ctx.restore();
+  drawHealth(me.x-camX, me.y-camY, 48, me.hp, me.maxHp);
 }
 loop();
